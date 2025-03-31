18b9820098036e07471624cb0dbb90c6bcac29af
Mayara Araujo <202502043368@alunos.estacio.br>
2025-03-31 13:45:36 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,42 +1,69 @@
 #include <stdio.h>
 
-int main() {
-    // Movimento da Torre (usando for)
-    printf("Movimento da Torre:\n");
-    for (int i = 0; i < 5; i++) {
+// Função recursiva para o movimento da Torre
+void movimentoTorre(int casas) {
+    if (casas > 0) {
         printf("Direita\n");
+        movimentoTorre(casas - 1);
     }
+}
 
-    printf("\n");
-
-    // Movimento do Bispo (usando while)
-    printf("Movimento do Bispo:\n");
-    int j = 0;
-    while (j < 5) {
+// Função recursiva para o movimento do Bispo
+void movimentoBispoRecursivo(int casas) {
+    if (casas > 0) {
         printf("Cima, Direita\n");
-        j++;
+        movimentoBispoRecursivo(casas - 1);
     }
+}
 
-    printf("\n");
-
-    // Movimento da Rainha (usando do-while)
-    printf("Movimento da Rainha:\n");
-    int k = 0;
-    do {
+// Função recursiva para o movimento da Rainha
+void movimentoRainha(int casas) {
+    if (casas > 0) {
         printf("Esquerda\n");
-        k++;
-    } while (k < 8);
+        movimentoRainha(casas - 1);
+    }
+}
 
+// Movimento do Bispo com loops aninhados
+void movimentoBispoLoops(int casas) {
+    for (int i = 0; i < casas; i++) {
+        for (int j = 0; j < 1; j++) {
+            printf("Cima, Direita\n");
+        }
+    }
+}
+
+int main() {
+    // Movimento da Torre (usando recursão)
+    printf("Movimento da Torre:\n");
+    movimentoTorre(5);
     printf("\n");
 
-    // Movimento do Cavalo (usando loops aninhados)
-    printf("Movimento do Cavalo:\n");
+    // Movimento do Bispo (usando recursão)
+    printf("Movimento do Bispo (Recursão):\n");
+    movimentoBispoRecursivo(5);
+    printf("\n");
 
-    // Movimento em "L": duas casas para baixo e uma para a esquerda
-    for (int i = 0; i < 2; i++) {
-        printf("Baixo\n");
+    // Movimento do Bispo (usando loops aninhados)
+    printf("Movimento do Bispo (Loops aninhados):\n");
+    movimentoBispoLoops(5);
+    printf("\n");
+
+    // Movimento da Rainha (usando recursão)
+    printf("Movimento da Rainha:\n");
+    movimentoRainha(8);
+    printf("\n");
+
+    // Movimento do Cavalo (usando loops complexos)
+    printf("Movimento do Cavalo:\n");
+    for (int i = 0; i < 3; i++) {
+        if (i < 2) {
+            printf("Cima\n");
+        } else {
+            printf("Direita\n");
+        }
     }
-    printf("Esquerda\n");
+    printf("\n");
 
     return 0;
 }
